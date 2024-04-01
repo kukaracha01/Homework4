859d7f8d65aa252c3f646ebd5e8a8eb59ecd7638
mu <semkadarov@gmail.com>
2024-04-01 22:33:32 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        System.out.println("Task2");
+
 
     }
 }
