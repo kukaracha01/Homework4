4835d0bd08459043900822ff4532de9bbbb1dd04
mu <semkadarov@gmail.com>
2024-04-04 21:18:04 +0300

    Ну точно все

--- src/Main.java
+++ src/Main.java
@@ -87,5 +87,13 @@ public class Main {
         else {
             System.out.println("Какие то два числа равны друг другу");
         }
+
+        System.out.println("Task1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
